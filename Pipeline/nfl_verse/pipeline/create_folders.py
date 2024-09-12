0ca00aacaea7b8f46e40c0168d9e7763b1baa81a
nickmcgourthy <nickmcgourthy@gmail.com>
2024-09-12 14:24:16 -0400

    Folder functionality - Creating team and week folders for a season.

--- Pipeline/nfl_verse/pipeline/create_folders.py
+++ Pipeline/nfl_verse/pipeline/create_folders.py
@@ -1,5 +1,6 @@
 import os 
 from Pipeline.nfl_verse.helpers.get_teams import get_teams
+import pandas as pd
 
 def make_subfolders(dir, subfolder_name, subfolders):
     subfolder_dir = os.path.join(dir, subfolder_name)
@@ -9,9 +10,23 @@ def make_subfolders(dir, subfolder_name, subfolders):
         sub_dir = os.path.join(subfolder_dir, subfolder)
         os.makedirs(sub_dir)
 
-def create_folder(year):
-    directory = os.path.join("Clean_Data/", str(year))
-    os.makedirs(directory)
+def get_max_week(df):
+    week = df[df['season_type'] == 'REG']['week'].max()
+    return week
+
+def create_week_folders(week, directory):
+    week_directory = os.path.join(directory, 'Weeks')
+    os.makedirs(week_directory)
+
+    for i in range(1, (week+1)):
+        current_week_directory = os.path.join(week_directory, str(i))
+        os.makedirs(current_week_directory)
+    print("Week folders created")
+
+def create_team_folders(directory):
+    team_directory = os.path.join(directory, "Teams")
+
+    os.makedirs(team_directory)
 
     offense_subfolders = ['Passing', 'Rushing', 'Conversions']
     defense_subfolders = ['Passing', 'Rushing', 'Conversions']
@@ -20,8 +35,21 @@ def create_folder(year):
     teams = get_teams()
 
     for team in teams:
-        team_dir = os.path.join(directory, team)
+        team_dir = os.path.join(team_directory, team)
         os.makedirs(team_dir)
         make_subfolders(team_dir, 'Offense', offense_subfolders)
         make_subfolders(team_dir, 'Defense', defense_subfolders)
         make_subfolders(team_dir, 'Players', player_subfolders)
+
+    print("Team folders created")
+
+def create_folders(year, df):
+    directory = os.path.join("Clean_Data/", str(year))
+    
+    print("Creating team folders")
+    create_team_folders(directory)
+
+    max_week = get_max_week(df)
+
+    print("Creating week folders")
+    create_week_folders(max_week, directory)
--- Pipeline/organize_pbp.py
+++ Pipeline/organize_pbp.py
@@ -1,7 +1,77 @@
 # python3 -m Pipeline.organize_pbp
 
 import os 
+import nfl_data_py as nfl
+import pandas as pd
+from Pipeline.nfl_verse.pipeline.create_folders import create_folders
+from Pipeline.nfl_verse.pipeline.create_folders import get_max_week
 
-from Pipeline.nfl_verse.pipeline.create_folders import create_folder
+play_cols = [
+    "play_id",
+    "game_id",
+    "home_team",
+    "away_team",
+    "season_type",
+    "week",
+    "posteam",
+    "drive",
+    "qtr",
+    "time",
+    "down",
+    "goal_to_go",
+    "ydstogo",
+    "yrdln",
+    "desc",
+    "play_type",
+    "yards_gained",
+    "shotgun",
+    "qb_dropback",
+    "qb_kneel",
+    "qb_spike",
+    "qb_scramble",
+    "air_yards",
+    "yards_after_catch",
+    "run_location",
+    "run_gap",
+    "total_home_score",
+    "total_away_score",
+    "fg_prob",
+    "td_prob",
+    "ep",
+    "epa",
+    "incomplete_pass",
+    "interception",
+    "solo_tackle",
+    "pass_attempt",
+    "rush_attempt",
+    "penalty",
+    "qb_hit",
+    "sack",
+    "touchdown",
+    "roof",
+    "temp",
+    "wind",
+    "penalty_team",
+    "penalty_yards",
+    "two_point_conv_result",
+    "fixed_drive",
+    "drive_quarter_start",
+    "drive_quarter_end",
+    "drive_first_downs",
+    "drive_inside20",
+    "drive_ended_with_score",
+    "drive_end_transition",
+    "drive_start_yard_line",
+    "first_down",
+    "score_differential"
+]
 
-create_folder(2023)
+year = 2023
+
+print("Importing pbp data...")
+#df = nfl.import_pbp_data(years=[year], downcast=False, columns=play_cols)
+df = pd.read_csv("pbp.csv")
+#df.to_csv("pbp.csv", index=False)
+
+print("Calling create_folders")
+create_folders(year, df)
